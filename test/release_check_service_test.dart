1bbf5d74d676802fc40aa996a5c55d950fac1161
privacyOG <kriptiik@proton.me>
2026-06-15 11:46:07 +1000

    Test release version comparison

--- test/release_check_service_test.dart
+++ test/release_check_service_test.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pfsense_manager/services/release_check_service.dart';
 
 void main() {
-  test('placeholder', () {
-    expect(true, isTrue);
+  test('compares release versions safely', () {
+    expect(ReleaseCheckService.isNewer('v1.7.1', '1.7.0'), isTrue);
+    expect(ReleaseCheckService.isNewer('v1.7.0', '1.7.0'), isFalse);
+    expect(ReleaseCheckService.isNewer('v1.8.0', '1.7.1+10'), isTrue);
+    expect(ReleaseCheckService.isNewer('latest', '1.7.0'), isFalse);
   });
 }
