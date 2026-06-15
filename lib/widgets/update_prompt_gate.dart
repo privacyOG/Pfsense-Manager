e02063b741ff74f6dc0d4ddd7a239907b45511e7
privacyOG <kriptiik@proton.me>
2026-06-15 11:24:07 +1000

    Add update prompt container

---
+++ lib/widgets/update_prompt_gate.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class UpdatePromptGate extends StatelessWidget {
+  const UpdatePromptGate({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => child;
+}
