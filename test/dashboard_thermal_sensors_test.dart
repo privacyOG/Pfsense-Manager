e39d2b57c688e01fe0584ab90b5060f67273a102
privacyOG <kriptiik@proton.me>
2026-06-16 14:27:26 +1000

    Cover pfSense per-core temperature fields

--- test/dashboard_thermal_sensors_test.dart
+++ test/dashboard_thermal_sensors_test.dart
@@ -42,6 +42,27 @@ void main() {
       expect(data.temperatureC, 53);
     });
 
+    test('parses four pfSense CPU sensors without unit duplicates', () {
+      final data = DashboardData.fromJson({
+        'cpu_usage': 7,
+        'mem_usage': 11,
+        'uptime': '2 days',
+        'thermal_sensors': {
+          'dev.cpu.0.temperature': {'c': 35.7, 'f': 96.3},
+          'dev.cpu.1.temperature': {'c': 35.7, 'f': 96.3},
+          'dev.cpu.2.temperature': {'c': 35.7, 'f': 96.3},
+          'dev.cpu.3.temperature': {'c': 35.7, 'f': 96.3},
+        },
+      });
+
+      expect(
+        data.thermalSensors.map((sensor) => sensor.name).toList(),
+        ['CPU 0', 'CPU 1', 'CPU 2', 'CPU 3'],
+      );
+      expect(data.thermalSensors.any((sensor) => sensor.name == 'F'), isFalse);
+      expect(data.temperatureC, 35.7);
+    });
+
     test('keeps legacy single temperature support', () {
       final data = DashboardData.fromJson({
         'cpu_usage': 12,
