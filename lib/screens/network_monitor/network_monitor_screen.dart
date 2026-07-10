446a35edb1be38b010307a0c22b9b67c61375f0b
privacyOG <kriptiik@proton.me>
2026-07-11 06:39:42 +1000

    Add network monitor implementation library

---
+++ lib/screens/network_monitor/network_monitor_screen.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../models/dashboard.dart';
+import '../../models/network_state.dart';
+import '../../providers/session_provider.dart';
+import '../../widgets/interface_traffic_totals.dart';
+
+part 'network_monitor_state.dart';
+part 'network_monitor_widgets.dart';
