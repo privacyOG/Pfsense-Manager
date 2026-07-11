ee530cd45a744718a06c904941c612b63a61f9ec
privacyOG <kriptiik@proton.me>
2026-07-12 08:34:54 +1000

    Import material icons for system log source tests

--- test/system_log_source_test.dart
+++ test/system_log_source_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pfsense_manager/models/system_log_source.dart';
 import 'package:pfsense_manager/screens/system_logs_screen.dart';
